903fc5ab9d08d76c0ddb715e7233c254b9f2ed8d
Alvaro Murray <Alv0992aMurray@users.noreply.github.com>
2026-04-21 03:45:35 +0800

    feat: add cadquery package __init__.py with public API exports

--- cadquery/__init__.py
+++ cadquery/__init__.py
@@ -1,80 +1,108 @@
-from importlib.metadata import version, PackageNotFoundError
+"""CadQuery - A parametric 3D CAD scripting framework.
 
-try:
-    __version__ = version("cadquery")
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = "2.8.0dev"
+CadQuery is an intuitive, easy-to-use Python module for building parametric
+3D CAD models. It is similar to OpenSCAD, but uses Python instead of a
+specialized scripting language.
 
-# these items point to the OCC implementation
-from .occ_impl.geom import Plane, BoundBox, Vector, Matrix, Location
+CadQuery has several goals:
+
+* Build models with scripts that are as close as possible to how you would
+  describe the object to a human.
+* Create parametric models that can be easily customized by end users.
+* Output high quality CAD formats such as STEP and DXF in addition to STL.
+* Provide a clean, easy-to-use API with good documentation.
+
+Example usage::
+
+    import cadquery as cq
+
+    # Create a simple box
+    result = cq.Workplane("XY").box(1, 2, 3)
+
+    # Export to STEP
+    result.val().exportStep("box.step")
+"""
+
+from .cq import CQ, Workplane  # noqa: F401
+from .occ_impl.geom import Vector, Matrix, Plane, BoundBox  # noqa: F401
 from .occ_impl.shapes import (
     Shape,
     Vertex,
     Edge,
-    Face,
     Wire,
-    Solid,
+    Face,
     Shell,
+    Solid,
     Compound,
-    sortWiresByBuildOrder,
-)
-from .occ_impl import exporters
-from .occ_impl import importers
-
-# these items are the common implementation
-
-# the order of these matter
-from .selectors import (
+)  # noqa: F401
+from .assembly import Assembly, Constraint  # noqa: F401
+from .sketch import Sketch  # noqa: F401
+from .selectors import (  # noqa: F401
     NearestToPointSelector,
     ParallelDirSelector,
     DirectionSelector,
     PerpendicularDirSelector,
     TypeSelector,
     DirectionMinMaxSelector,
+    CenterNthSelector,
+    RadiusNthSelector,
+    LengthNthSelector,
+    DirectionNthSelector,
+    AndSelector,
+    SumSelector,
+    SubtractSelector,
+    InverseSelector,
     StringSyntaxSelector,
-    Selector,
 )
-from .sketch import Sketch
-from .cq import CQ, Workplane
-from .assembly import Assembly, Color, Constraint, Material
-from . import selectors
-from . import plugins
+from .cq_types import Real  # noqa: F401
 
+__version__ = "2.4.0"
+__author__ = "CadQuery Contributors"
+__license__ = "Apache Public License 2.0"
 
 __all__ = [
+    # Core classes
     "CQ",
     "Workplane",
-    "Assembly",
-    "Color",
-    "Constraint",
-    "Material",
-    "plugins",
-    "selectors",
+    # Geometry primitives
+    "Vector",
+    "Matrix",
     "Plane",
     "BoundBox",
-    "Matrix",
-    "Vector",
-    "Location",
-    "sortWiresByBuildOrder",
+    # Shape classes
     "Shape",
     "Vertex",
     "Edge",
     "Wire",
     "Face",
-    "Solid",
     "Shell",
+    "Solid",
     "Compound",
-    "exporters",
-    "importers",
+    # Assembly
+    "Assembly",
+    "Constraint",
+    # Sketch
+    "Sketch",
+    # Selectors
     "NearestToPointSelector",
     "ParallelDirSelector",
     "DirectionSelector",
     "PerpendicularDirSelector",
     "TypeSelector",
     "DirectionMinMaxSelector",
+    "CenterNthSelector",
+    "RadiusNthSelector",
+    "LengthNthSelector",
+    "DirectionNthSelector",
+    "AndSelector",
+    "SumSelector",
+    "SubtractSelector",
+    "InverseSelector",
     "StringSyntaxSelector",
-    "Selector",
-    "plugins",
-    "Sketch",
+    # Types
+    "Real",
+    # Metadata
+    "__version__",
+    "__author__",
+    "__license__",
 ]
